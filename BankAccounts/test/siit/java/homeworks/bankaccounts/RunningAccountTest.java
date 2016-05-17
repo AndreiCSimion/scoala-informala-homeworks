e90aa7d6bf5e6182456ee660866b07cc38b3b0ab
AndreiCSimion <andrei_simion_89@yahoo.com>
2016-05-18 00:29:58 +0300

    Update RunningAccountTest.java

--- BankAccounts/test/siit/java/homeworks/bankaccounts/RunningAccountTest.java
+++ BankAccounts/test/siit/java/homeworks/bankaccounts/RunningAccountTest.java
@@ -1,7 +1,7 @@
 /**
  * This class is a test class for the {@link RunningAccount} class.
  * <p>
- * @author Adelina Cujba
+ * @author Andrei Simion
  */
 package siit.java.homeworks.bankaccounts;
 
@@ -18,21 +18,20 @@ public class RunningAccountTest {
 		// given
 		float monthlyFee = 5;
 		Date accountCreationDate = TestUtils.getDate(2016, 1, 1);
-		float credit = 800;
+		float credit = 2000;
 		RunningAccount runningAccount = new RunningAccount(accountCreationDate, monthlyFee, credit);
 
 		// when
-		runningAccount.withdrawFunds(50);
 		runningAccount.withdrawFunds(100);
-		runningAccount.addFunds(80);
+		runningAccount.withdrawFunds(200);
 		Date balanceInterogationTime = TestUtils.getDate(2016, 5, 1);
 		float actualBalance = runningAccount.getBalance(balanceInterogationTime);
 
 		// then
-		float withdrawalsAndAdds = credit - 50 - 100 + 80;
+		float withdrawalsAndAdds = credit - 200 - 100;
 		float expectedBalanceAfterOperations = credit - withdrawalsAndAdds;
 
-		assertEquals("4 months worth of withdrawals and adds have been deducted.", 730,
+		assertEquals("4 months worth of withdrawals and adds have been deducted.", 700,
 				actualBalance, 0);
 	}
 
